4143a40af25c39d73a9fb37583089ffb9c0ed40c
Afreen Ali <afreen.a1808@gmail.com>
2026-07-26 11:31:32 +0530

    bug fixing

--- src/main/java/com/crochet/crochetstore/model/Payment.java
+++ src/main/java/com/crochet/crochetstore/model/Payment.java
@@ -10,12 +10,7 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", nullable = false)
-    private Order order;
-
-    @Column(unique = true)
-    private String razorpayOrderId;
+    private String orderId;
 
     private String paymentId;
 
@@ -23,48 +18,5 @@ public class Payment {
 
     private Double amount;
 
-    public Long getId() {
-        return id;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    public String getRazorpayOrderId() {
-        return razorpayOrderId;
-    }
-
-    public void setRazorpayOrderId(String razorpayOrderId) {
-        this.razorpayOrderId = razorpayOrderId;
-    }
-
-    public String getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(String paymentId) {
-        this.paymentId = paymentId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
+    // getters & setters
 }
